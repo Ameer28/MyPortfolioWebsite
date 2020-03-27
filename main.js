358037d6d5bb02c67f5519fe6e2ef28b6827f30c
Ameer28 <elearningviet88@gmail.com>
2020-03-27 15:02:48 +0800

    My Portfolio Website As At 27 Mar 2020

--- main.js
+++ main.js
@@ -1,25 +1,42 @@
-$('#exampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var recipient = button.data('whatever') // Extract info from data-* attributes
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this)
-    modal.find('.modal-title').text('New message to ' + recipient)
-    modal.find('.modal-body input').val(recipient)
+$('#writeToMeModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget)
+  var recipient = button.data('whatever')
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
 })
 
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      var forms = document.getElementsByClassName('formModalValidation');
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
+const writeToMeForm = document.getElementById('writeToMeForm');
+const nameField = document.getElementById('nameField');
+const emailField = document.getElementById('emailField');
+const submitButton = document.getElementById('submitButton');
+
+nameField.addEventListener('keyup', function () {
+  isValidName = nameField.checkValidity();
+
+  if (isValidName) {
+    submitButton.disable = false;
+  } else {
+    submitButton.disable = true;
+  }
+});
+
+emailField.addEventListener('keyup', function (event) {
+  isValidEmail = emailField.checkValidity();
+
+  if (isValidEmail) {
+    submitButton.disable = false;
+  } else {
+    submitButton.disable = true;
+  }
+});
+
+submitButton.addEventListener('click', function (event) {
+  writeToMeForm.submit();
+});
+
+$('.imgFadeHover').hover(function () {
+  $(this).siblings().addClass('fadeHover');
+}, function () {
+  $(this).siblings().removeClass('fadeHover');
+});
